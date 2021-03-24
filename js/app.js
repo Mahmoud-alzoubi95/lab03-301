e2f334587a7c85de53518b8d45ed9c771b6b989a
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-03-24 21:28:57 +0200

    adding sort

--- js/app.js
+++ js/app.js
@@ -25,54 +25,125 @@ function Animals(animals) {
     containerPerson.attr("class", `${this.keyword} filter`);
     $('main').append(containerPerson);
 
-    this.optionRender();
+    // this.optionRender();
 }
 
-Animals.prototype.optionRender=function() {
+// Animals.prototype.optionRender=function() {
 
-let opt = `<option>${this.keyword}</option>`
+// let opt = `<option>${this.keyword}</option>`
 
-if(arrofOption.includes(this.keyword)===true){}
+// if(arrofOption.includes(this.keyword)===true){}
+// else{
+//   arrofOption.push(this.keyword);
+//   $(".select").append(opt);
+// }
+// }
+
+
+function optionListRender(myImages,myKeyWord){
+  myImages.forEach((animals)=>{
+if(myKeyWord.includes(animals.keyword)===true){}
 else{
-  arrofOption.push(this.keyword);
+  myKeyWord.push(animals.keyword);
+  const opt = `<option>${animals.keyword}</option>`
   $(".select").append(opt);
 }
+});
 }
-
-
   const ajaxData = {
     method: "get",
     dataType: "json",
   };
   
-
 let numOfPagge = $('button').on('click',function(){
-
+  $('div').hide();
   numOfPagge = $(this).attr('id')
-  
+  // renderFilterhorns(selectKeyword);
   ajaxFunc(numOfPagge);
+  
   
 })
 
 ajaxFunc(1);
 
 function ajaxFunc(numOfPagge){
-  
+  const myImages =[];
+  const myKeyWord =[];
+
   $.ajax(`data/page-${numOfPagge}.json`, ajaxData).then((data) => {
     data.forEach((animals) => {
       let titleObject = new Animals(animals);
-      console.log(titleObject);
-      // dogObject.renderManually();
-       titleObject.render();
-    });
-  });
+      myImages.push(titleObject);
 
+      // console.log(myImages);
+      // dogObject.renderManually();
+      // titleObject.optionRender();
+      titleObject.render();
+    });
+
+    optionListRender(myImages,myKeyWord);
+    // console.log(myKeyWord);
+  });
 }
+
+
+
+$('#select2').on('click', function() {
+
+  let selectedoption = $('#select2').val();
+  // let selectedoption1 = $('#select1').val();
+
+  if (selectedoption == 'byTitle') {
+    arrOfprop.sort((a, b) => {
+          if (a.title.toUpperCase() > b.title.toUpperCase()) return 1;
+          else if (a.title < b.title) return -1;
+          else return 0;
+      });
+      // arrOfprop;
+      // render2 to do the render
+      // render2($('#select1').val());//
+      render2(selectedoption1); //Where "selectedoption1" is the value of the filter
+
+  } else if (selectedoption == 'byHorn') {
+    arrOfprop.sort((a, b) => {
+          if (a.horns > b.horns) return 1;
+          else if (a.horns < b.horns) return -1;
+          else return 0;
+      });
+      // arrOfprop;
+      render2(selectedoption1);
+      // let selectedoption = $('#select1').val();
+      // render2(selectedoption);
+      // render2($('#select1').val());
+
+  }
+  console.log(arrOfprop);
+
+});
+
+
+function render2(value) {
+
+  // To initiat the render for the selected keyword;
+  $('section').empty();
+
+  for (var i = 0; i < arrOfprop.length; i++) {
+      if ((arrOfprop[i].keyword) == value || value == 'filter by keword') {
+
+          let musTemplate = $('#templete').html();
+          let newObj = Mustache.render(musTemplate, arrOfprop[i]);
+          $('section').append(newObj);
+
+      }
+  }
+}
+
+
 
   $(".select").on("change",function(event){
     let selectKeyword=this.value;
     if(selectKeyword!=="select"){
-      console.log(selectKeyword);
+      // console.log(selectKeyword);
       renderFilterhorns(selectKeyword);
     }
   });
@@ -87,6 +158,4 @@ else{
   $(`.${kind.keyword}`).removeClass("filter")
 }
   })
-}
-
-
+  }
